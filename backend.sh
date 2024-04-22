04747d4d9784affadffa80c9ceb6776151e5ab73
vikram1503 <vikram15031989@gmail.com>
2024-04-22 23:52:30 +0530

    testing

--- backend.sh
+++ backend.sh
@@ -7,6 +7,9 @@ r="\e[31m"
 g="\e[32m"
 y="\e[33m"
 n="\e[0m"
+echo "please enter db password:"
+read -s mysql_root_password
+
 
 validate(){
     if [ $1 -ne 0 ]
@@ -42,5 +45,39 @@ validate $? "creating expense user"
 else
 echo -e "expense user already created...$y skipping $n"
 fi
+
+mkdir -p /app &>>$logfile
+validate $? "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$logfile
+validate $? "downloading backend code"
+
+cd /app 
+unzip /tmp/backend.zip &>>$logfile
+validate $? "extracted backend code"
+
+npm install &>>$logfile
+validate $? "installing nodejs dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$logfile
+ validate $? "copied backend service"
+
+ systemctl daemon-reload &>>$logfile
+validate $? "daemon reload"
+
+ systemctl start backend &>>$logfile
+ validate $? "starting backend"
+ 
+ systemctl enable backend &>>$logfile
+ validate $? "enabling backend"
+
+ dnf install mysql -y &>>$logfile
+ validate $? "installing mysql client"
+
+ mysql -h db.imvicky.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$logfile
+validate $? "schema loading"
+
+systemctl restart backend &>>$logfile
+validate $? "restarting backend"
 
 

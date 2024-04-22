f8374adde07aca3408c50f3be61b7eec424ccb4e
vikram1503 <vikram15031989@gmail.com>
2024-04-22 22:45:21 +0530

    testing

--- mysql.sh
+++ mysql.sh
@@ -8,7 +8,7 @@ g="\e[32m"
 y="\e[33m"
 n="\e[0m"
 echo "please enter db password:"
-read -s mysql root password
+read -s mysql_root_password
 validate(){
     if [ $1 -ne 0 ]
     then echo -e "$2...$r failure $n"
@@ -36,10 +36,10 @@ validate $? "starting mql server"
  
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$logfile
 # validate $? "setting up root password "
-mysql -h db.imvicky.online -uroot -p${mysql root password} -e 'show databases;'  &>>$logfile
+mysql -h db.imvicky.online -uroot -p${mysql _root_password} -e 'show databases;'  &>>$logfile
 if [ $? -ne 0 ]
 then 
-   mysql_secure_installation --set-root-pass ${mysql root password} &>>$logfile
+   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$logfile
    validate $? "mysql root password setup"
    else 
    echo -e "mysql root password is already set...$y skipping $n"
